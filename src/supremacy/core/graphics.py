b7f38a81dbe19679d92f73a472ebe8078524cac5
Neil Vaytet <neil.vaytet@esss.se>
2023-01-28 13:23:12 +0100

    add shapes

--- src/supremacy/core/graphics.py
+++ src/supremacy/core/graphics.py
@@ -21,5 +21,25 @@ class Graphics:
         self.screen.bgpic('background.png')
         cv.itemconfig(self.screen._bgpic, anchor="sw")
 
+        tank = ((-4, -5), (4, -5), (4, 5), (1, 5), (1, 10), (-1, 10), (-1, 5),
+                (-4, 5))
+        ship = ((-4, -5), (4, -5), (4, 5), (0, 10), (-4, 5))
+        jet = ((-4, -5), (4, -5), (1, -4), (1, -2), (8, -2), (1, 3), (0, 10),
+               (-1, 3), (-8, -2), (-1, -2), (-1, -4))
+
+        self.screen.register_shape("tank", tank)
+        self.screen.register_shape("ship", ship)
+        self.screen.register_shape("jet", jet)
+
+        # self.screen.register_shape("jet.gif")
+
+        # self.t = turtle.Turtle()
+
+        # self.t.shape("jet.gif")
+        # self.t.setheading(45)
+        # self.t.forward(500)
+        # self.t.setheading(90)
+        # self.t.forward(200)
+
 
 #
