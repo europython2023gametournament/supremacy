a0b89aef73f192fd600e8da24490f6d31828eae4
Neil Vaytet <neil.vaytet@esss.se>
2023-07-16 11:17:43 +0200

    start caching images

--- src/supremacy/config.py
+++ src/supremacy/config.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from typing import Any, List, Tuple
+import os
 
 import importlib_resources as ir
 import numpy as np
@@ -66,6 +67,7 @@ class Config:
         self.taskbar_height = 60
         self.fps = 15
         self.resources = ir.files("supremacy") / "resources"
+        self.cache_dir = ".cache"
         file = font_manager.findfont("sans")
         self.small_font = ImageFont.truetype(file, size=10)
         self.large_font = ImageFont.truetype(file, size=16)
@@ -87,12 +89,19 @@ class Config:
         screen_width = screen.width - self.scoreboard_width
         screen_height = screen.height - dy
         self.scaling = min(min(screen_width / self.nx, screen_height / self.ny), 1.0)
+        self.scaling_str = f"{self.scaling:.3f}"
 
         self.generate_images(nplayers)
 
     def generate_images(self, nplayers: int):
-        img = Image.open(self.resources / "explosion.png")
-        self.images = {"explosion": _to_image(scale_image(img, self.scaling))}
+        expl_fname = os.path.join(self.cache_dir, f"explosion_{self.scaling_str}.png")
+        if os.path.exists(expl_fname):
+            expl_img = Image.open(expl_fname)
+        else:
+            img = Image.open(self.resources / "explosion.png")
+            expl_img = _to_image(scale_image(img, self.scaling))
+            expl_img.save(expl_fname)
+        self.images = {"explosion": expl_img}
         self.colors = _make_colors(nplayers)
         for n in range(nplayers):
             rgb = self.colors[n]
@@ -103,33 +112,68 @@ class Config:
     def generate_vehicle_images(self, n: int, rgb: tuple):
         for name in ("jet", "ship", "tank"):
             for health in range(0, MAX_HEALTH + 1, 10):
-                img = _make_base_image(self.resources, name, rgb)
-                d = ImageDraw.Draw(img)
-                d.text(
-                    (img.width / 2, img.height / 2),
-                    str(health),
-                    fill=(0, 0, 0),
-                    font=self.small_font,
-                    anchor="mm",
+                img_name = f"{name}_{n}_{health}"
+                img_fname = os.path.join(
+                    self.cache_dir, f"{img_name}_{self.scaling_str}.png"
                 )
-                self.images[f"{name}_{n}_{health}"] = _to_image(
-                    scale_image(img, self.scaling)
-                )
+                if os.path.exists(img_fname):
+                    img = Image.open(img_fname)
+                else:
+                    img = _make_base_image(self.resources, name, rgb)
+                    d = ImageDraw.Draw(img)
+                    d.text(
+                        (img.width / 2, img.height / 2),
+                        str(health),
+                        fill=(0, 0, 0),
+                        font=self.small_font,
+                        anchor="mm",
+                    )
+                    img = _to_image(scale_image(img, self.scaling))
+                    img.save(img_fname)
+                self.images[img_name] = img
 
     def generate_base_images(self, n: int, rgb: tuple):
         name = "base"
-        img = _make_base_image(self.resources, name, rgb)
-        self.images[f"player_{n}"] = scale_image(img, self.scaling)
-        self.images[f"{name}_{n}"] = _to_image(scale_image(img, self.scaling))
-        d = ImageDraw.Draw(img)
-        d.text(
-            (img.width / 2, img.height / 2),
-            "C",
-            fill=(0, 0, 0),
-            font=self.large_font,
-            anchor="mm",
+        player_img_name = f"player_{n}"
+        player_img_fname = os.path.join(
+            self.cache_dir, f"{player_img_name}_{self.scaling_str}.png"
         )
-        self.images[f"{name}_{n}_C"] = _to_image(scale_image(img, self.scaling))
+        if os.path.exists(player_img_fname):
+            player_img = Image.open(player_img_fname)
+        else:
+            img = _make_base_image(self.resources, name, rgb)
+            player_img = scale_image(img, self.scaling)
+        self.images[player_img_name] = player_img
+
+        name_img_name = f"{name}_{n}"
+        name_img_fname = os.path.join(
+            self.cache_dir, f"{name_img_name}_{self.scaling_str}.png"
+        )
+        if os.path.exists(name_img_fname):
+            name_img = Image.open(name_img_fname)
+        else:
+            img = _make_base_image(self.resources, name, rgb)
+            name_img = _to_image(scale_image(img, self.scaling))
+        self.images[name_img_name] = name_img
+
+        name_c_img_name = f"{name}_{n}_C"
+        name_c_img_fname = os.path.join(
+            self.cache_dir, f"{name_c_img_name}_{self.scaling_str}.png"
+        )
+        if os.path.exists(name_c_img_fname):
+            name_c_img = Image.open(name_c_img_fname)
+        else:
+            img = _make_base_image(self.resources, name, rgb)
+            d = ImageDraw.Draw(img)
+            d.text(
+                (img.width / 2, img.height / 2),
+                "C",
+                fill=(0, 0, 0),
+                font=self.large_font,
+                anchor="mm",
+            )
+            name_c_img = _to_image(scale_image(img, self.scaling))
+        self.images[name_c_img_name] = name_c_img
 
         for health in range(0, MAX_HEALTH + 1, 10):
             img = Image.new("RGBA", (24, 24), (0, 0, 0, 0))
